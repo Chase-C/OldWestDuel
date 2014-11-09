15f7e2e97cc3c239e910af1df0d09d3a08bf2e8b
TheBlueGamer55 <endless.answers@gmail.com>
2014-11-08 17:22:54 -0800

    Added screen shaking functionality

--- Engine.js
+++ Engine.js
@@ -13,6 +13,13 @@ var Engine = function(w, h)
     this.player2 = new Player(670, 340, true);
 
     this.winner = 0;
+	
+	this.isScreenShaking = false; //set this to true any time a screen shake should occur 
+	this.isScreenShakingEnd = false;
+	this.screenShakeTimer = 0;
+	this.shakeMagnitude = 12; //how far away the camera shakes around its original point, in pixels
+	this.transX = 0; //keeps track of the canvas's translation in order to reset it to its original position after screen shaking
+	this.transY = 0;
 }
 
 Engine.prototype =
@@ -27,6 +34,16 @@ Engine.prototype =
         var dt = currTime - this.time;
 
         this.time = currTime;
+		
+		//code for screen shaking
+		if(this.isScreenShaking){
+			this.screenShakeTimer += dt;
+			if(this.screenShakeTimer >= 1000){ //how many milliseconds the screen shaking lasts
+				this.screenShakeTimer = 0;
+				this.isScreenShaking = false;
+				this.isScreenShakingEnd = true;
+			}
+		}
     },
 
     // Reset the simulation
@@ -48,6 +65,21 @@ Engine.prototype =
 
     draw: function(canvas)
     {
+		if(this.isScreenShaking){
+			canvas.translate(-this.transX, -this.transY); //move the canvas back to its origin (0, 0)
+			var newX = Math.random() * this.shakeMagnitude * 2 - this.shakeMagnitude;
+			var newY = Math.random() * this.shakeMagnitude * 2 - this.shakeMagnitude;
+			canvas.translate(newX, newY); //move the canvas to a new origin
+			this.transX = newX; //keep track of the current x and y translations
+			this.transY = newY;
+		}
+		if(this.isScreenShakingEnd){ //solves the issue where the last translation isn't called
+			canvas.translate(-this.transX, -this.transY);
+			this.isScreenShakingEnd = false;
+			this.transX = 0;
+			this.transY = 0;
+		}
+	
         canvas.clearRect(0, 0, this.w, this.h);
         canvas.drawImage(this.backgroundImage, 0, 0, 820, 640);
 
